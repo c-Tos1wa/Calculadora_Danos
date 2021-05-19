05f30936d45b246ced03e7fed9919448db3ad8ea
Cristina <cristoshiwassaki@gmail.com>
2021-05-19 16:13:40 -0300

    calculo do dano no js

--- calculadora/evento.js
+++ calculadora/evento.js
@@ -1,7 +1,10 @@
+let criaturaSelecionada
+let armaSelecionada
+
 function iniciar(){
      const criaturas = document.getElementsByClassName('terceira') 
      for (let i = 0; i < criaturas.length; i++){
-         const clique = criaturas[i]; //
+         const clique = criaturas[i]; 
          clique.addEventListener('click', selecionado) //em um clique, estiliza a classe de criaturas
      };
 
@@ -13,10 +16,11 @@ function iniciar(){
   
 }
 
+
 function selecionado(evento){
     const itemSelecionado = evento.target.parentElement 
 
-    const divPai = itemSelecionado.parentElement //divisoria raiz do item selecionado
+    const divPai = itemSelecionado.parentElement 
     const filho = divPai.children //cd um dos galhos da raiz
     for (let i=0; i < filho.length; i++){
         filho[i].classList.remove('marcado') //remover o box se tiver dois ao msm tempo
@@ -24,5 +28,39 @@ function selecionado(evento){
 
     itemSelecionado.classList.add('marcado') //coloca um box cujas características estão no css
 
-    const itemID = itemSelecionado.getAttribute('id');
+    var idElemento = itemSelecionado.id
+    var itemID = divPai.getAttribute('name')
+    
+    if (itemID == 'personagem'){
+        criaturaSelecionada = idElemento
+    }
+    else {
+        armaSelecionada = idElemento
+    }
 }
+
+
+function dano(evento){
+    let botaoDano = evento.target;
+    if (criaturaSelecionada == 'orc'){
+        vida = 80
+    }
+    else if (criaturaSelecionada == 'goblin'){
+        vida = 50
+    }
+    else {
+        vida = 35
+    };
+
+    var ataque = Math.floor(Math.random()*100) + 1;
+
+    if (vida > ataque){
+        alert('Não foi desta vez! Tente novamente')
+    }
+    else {
+        alert('Parabéns! '+ criaturaSelecionada + 'foi derrotado(a)')
+    }
+}
+
+
+
